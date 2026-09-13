cb951791fb1d484ca68a907f7a234dd3b1525d5f
namrtaThakur <namrtathakur488@gmail.com>
2026-09-13 18:43:04 +0530

    Bulk sync: 10 submissions

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        for(int rows = 0; rows < 9; rows++)
+        {
+            unordered_set<char> seen;
+            for(int i = 0; i < 9; i++)
+            {
+                if(board[rows][i] == '.')
+                continue;
+                if(seen.count(board[rows][i]))
+                return false;
+                seen.insert(board[rows][i]);
+            }
+        }
+
+        for(int cols = 0; cols < 9; cols++)
+        {
+            unordered_set<char> seen;
+            for(int i = 0; i < 9; i++)
+            {
+                if(board[i][cols] == '.')
+                continue;
+                if(seen.count(board[i][cols]))
+                return false;
+                seen.insert(board[i][cols]);
+            }
+        }
+
+        for(int square = 0; square < 9; square++)
+        {
+            unordered_set<char> seen;
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3;j++)
+                {
+                    int row = (square/3) * 3 + i;
+                    int col = (square%3) * 3 + j;
+                    if(board[row][col] == '.')
+                    continue;
+                    if(seen.count(board[row][col]))
+                    return false;
+                    seen.insert(board[row][col]);
+                }
+            }
+        }
+
+        return true;
+        
+    }
+};
